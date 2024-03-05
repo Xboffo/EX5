38535c9506889720c9efb11431efeaf79ce6afcf
kauet <kauet@DESKTOP-3CLGCSJ>
2024-03-05 19:50:04 -0300

    Adicionar arquivos de projeto.

---
+++ EX5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a string que deseja inverter: ");
+        string stringDigitada = Console.ReadLine();
+        string stringInvertida = InverterString(stringDigitada);
+        Console.WriteLine("String invertida: " + stringInvertida);
+    }
+    static string InverterString(string stringDigitada)
+    {
+        char[] charArray = stringDigitada.ToCharArray();
+        int tamanho = charArray.Length;
+        for (int i = 0; i < tamanho / 2; i++)
+        {
+            // Inverte a String
+            char temp = charArray[i];
+            charArray[i] = charArray[tamanho - 1 - i];
+            charArray[tamanho - 1 - i] = temp;
+        }
+        return new string(charArray);
+    }
+}
